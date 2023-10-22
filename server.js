efd90344fe79fb7eb1ec5ce7e94df11a8e9396fb
Marija97 <marija.vujica97@gmail.com>
2023-10-22 23:54:00 +0200

    connection with mongodb database

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const errorHandler = require('./middleware/error_handler');
+const databaseConnect = require('./config/database_connection');
 const dotenv = require('dotenv').config();
 
+databaseConnect();
 const app = express();
-const port = process.env.port;
 
 app.use(express.json()); // middleware for bod json parsing
 app.use(errorHandler);   // middleware for handling errors
 
 app.use('/api/contacts', require('./routes/contacts'));
 
+const port = process.env.port;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
